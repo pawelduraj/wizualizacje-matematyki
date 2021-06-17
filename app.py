f08acbb33cc4e7e5793f7ea90f754d9c0c2ba9ee
Krzysztof Mizgała <km.2001@o2.pl>
2021-06-17 16:28:26 +0200

    Simple navbar and routing

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', content='Quiz')
+
+
+@app.route('/zagadnienie')
+def problem():
+    return render_template('index.html', content='Wybrane zagadnienie matematyczne')
+
+
+@app.route('/about')
+def about():
+    return render_template('index.html', content='O nas...')
 
 
 if __name__ == '__main__':
