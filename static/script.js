1ade72351ee5b647d7ad1e7ad261372f9aadc820
Krzysztof Mizgała <km.2001@o2.pl>
2021-06-30 15:39:42 +0200

    Validate question (only console.log)

--- static/script.js
+++ static/script.js
@@ -5,12 +5,17 @@ function check(zadanie) {
     }
     for (const element of document.getElementsByClassName(zadanie + ' correct')) {
         element.style.color = 'green';
+        if (element.previousElementSibling.checked) {
+            console.log(zadanie + ' is correct :)')
+        } else {
+            console.log(zadanie + ' is incorrect...')
+        }
     }
     for (const element of document.getElementsByClassName(zadanie + ' incorrect')) {
         element.style.color = 'red';
     }
-
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'),
         {
