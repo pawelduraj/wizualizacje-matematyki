7ea5b29f37c56c8087f7fab5f309eff7cad19ece
Krzysztof Mizgała <km.2001@o2.pl>
2021-06-30 14:59:24 +0200

    Colored radio inputs after question check

--- static/script.js
+++ static/script.js
@@ -1,10 +1,12 @@
 function check(zadanie) {
     // TODO funkcja sprawdzająca poprawność zadania
-    for (const element of document.getElementsByClassName(zadanie + '-correct')) {
+    for (const element of document.getElementsByClassName(zadanie + ' correct')) {
         element.style.color = 'green';
+        element.previousElementSibling.className = 'check';
     }
-    for (const element of document.getElementsByClassName(zadanie + '-incorrect')) {
+    for (const element of document.getElementsByClassName(zadanie + ' incorrect')) {
         element.style.color = 'red';
+        element.previousElementSibling.className = 'check';
     }
 }
 document.addEventListener('DOMContentLoaded', function() {
